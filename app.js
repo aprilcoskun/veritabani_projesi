6eb6614fd6e350ae5612f7b8532b17119fe2c7d2
alpcoskun <dev@alpcoskun.com>
2017-12-12 16:16:29 +0300

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+/*KUTUPHANELER*/
+const express = require('express');
+const exphbs = require('express-handlebars');
+const parser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const config = require('./config/default.json');
+/*GEREKLI DEGER ATAMALARI*/
+const PORT = 3000;
+const app = express();
+const router = express.Router();
+const server = http.createServer(app);
+
+/*HANDLEBARS KURULUMU*/
+app.engine('hbs', exphbs({
+  defaultLayout: "layout",
+  extname:'.hbs',
+  layoutsDir: `${__dirname}/views`
+}));
+app.set('view engine', 'hbs');
+
+/*STATIC DOSYALARIN(CSS, FOTOLAR) KURULUMU*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*MORGAN(LOGLAMA KUTUPHANESI) KURULUMU*/
+if (process.env.NODE_ENV !== 'production')
+  app.use(require('morgan')('dev'));
+
+/*HTTP BODY'SININ JSON OLARAK BELIRLENMESI*/
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+
+/*SERVER'IN 3000 PORTUNU DINLEMESI*/
+server.listen(PORT, () => {
+    console.log(`Listening on localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index',{deger:'sex',Admin:false});
+});
---
+++ public/scripts/main.js
@@ -0,0 +1,23 @@
+/*DOGUM TARIHI SINIRLARI(2-6 YAS)*/
+let minDate = new Date(new Date().setFullYear(new Date().getFullYear() - 6)).toJSON().split('T')[0];
+let maxDate = new Date(new Date().setFullYear(new Date().getFullYear() - 2)).toJSON().split('T')[0];
+document.getElementById('studentBday').setAttribute('min', minDate);
+document.getElementById('studentBday').setAttribute('max', maxDate);
+
+/*Birim Taksit Hesabi*/
+function calUnitPrice() {
+  /*max 5 hane kontrolu*/
+  if (document.getElementById('studentPrice').value.length > 5)
+      document.getElementById('studentPrice').value = document.getElementById('studentPrice').value.slice(0,5);
+  if (document.getElementById('studentAdvancePayment').value.length > 5)
+      document.getElementById('studentAdvancePayment').value = document.getElementById('studentAdvancePayment').value.slice(0,5);
+  /*Hesap*/
+  let price = document.getElementById('studentPrice').value;
+  let advancePayment = document.getElementById('studentAdvancePayment').value;
+  let unitSize = document.getElementById('studentPayNum').value;
+  if(price && advancePayment && unitSize && price > advancePayment)
+    document.getElementById('studentPayUnitPrice').value = Math.round(((price - advancePayment)/unitSize) * 100) / 100;
+  else
+    document.getElementById('studentPayUnitPrice').value = null;
+
+}
